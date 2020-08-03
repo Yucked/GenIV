b09b80072c41e71da1f7a19f1ce83f06b208ee86
Yucked <yucked@protonmail.com>
2020-08-02 22:25:24 -0400

    Extensions / Program.cs

---
+++ src/Extensions.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Colorful;
+using Discord;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Color = System.Drawing.Color;
+using Console = Colorful.Console;
+
+namespace GenIVIV {
+    public static class Extensions {
+        public const string MESSAGE_FORMAT = "[{0}] [{1}] [{2}]\n{3}";
+
+        public static LogLevel FromSeverityToLevel(this LogSeverity logSeverity) {
+            return (LogLevel) Math.Abs((int) logSeverity - 5);
+        }
+
+        public static Color GetLogLevelColor(this LogLevel logLevel) {
+            return logLevel switch {
+                LogLevel.Trace       => Color.LightBlue,
+                LogLevel.Debug       => Color.PaleVioletRed,
+                LogLevel.Information => Color.FromArgb(84, 194, 231),
+                LogLevel.Warning     => Color.Coral,
+                LogLevel.Error       => Color.Crimson,
+                LogLevel.Critical    => Color.Red,
+                LogLevel.None        => Color.Coral,
+                _                    => Color.White
+            };
+        }
+
+        public static string GetShortLogLevel(this LogLevel logLevel) {
+            return logLevel switch {
+                LogLevel.Trace       => "TRCE",
+                LogLevel.Debug       => "DBUG",
+                LogLevel.Information => "INFO",
+                LogLevel.Warning     => "WARN",
+                LogLevel.Error       => "EROR",
+                LogLevel.Critical    => "CRIT",
+                LogLevel.None        => "NONE",
+                _                    => "UKON"
+            };
+        }
+
+        public static IServiceCollection AddServices<TAttribute>(this IServiceCollection serviceCollection)
+            where TAttribute : Attribute {
+            var services = typeof(Program).Assembly
+                .GetTypes()
+                .Where(x => x.GetCustomAttribute<TAttribute>() != null);
+
+            foreach (var service in services) {
+                serviceCollection.AddSingleton(service);
+            }
+
+            return serviceCollection;
+        }
+
+        public static string GetNameOfT<T>() {
+            return $"{typeof(T).Name[..^9]}s";
+        }
+
+        public static bool TryTakeInput(string message, out string input) {
+            var formatters = new[] {
+                new Formatter('>', Color.Coral),
+                new Formatter(message, Color.White),
+            };
+
+            Console.WriteLineFormatted("{0} {1}", Color.White, formatters);
+            input = Console.ReadLine();
+            return !string.IsNullOrWhiteSpace(input);
+        }
+
+        public static void PrintInformationalHeader() {
+            const string LOGO =
+                @"
+  e88'Y88                  888 Y8b Y88888P 888 Y8b Y88888P 
+ d888  'Y   ,e e,  888 8e  888  Y8b Y888P  888  Y8b Y888P  
+C8888 eeee d88 88b 888 88b 888   Y8b Y8P   888   Y8b Y8P   
+ Y888 888P 888   , 888 888 888    Y8b Y    888    Y8b Y    
+  ""88 88""   ""YeeP"" 888 888 888     Y8P     888     Y8P     
+                
+";
+
+            Console.WriteWithGradient(LOGO, Color.Crimson, Color.HotPink);
+            Console.ReplaceAllColorsWithDefaults();
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+using GenIVIV.Attributes;
+using GenIVIV.Logging;
+using GenIVIV.Services;
+using GenIVIV.Services.Database;
+using GenIVIV.Services.Database.DataModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Victoria;
+using Console = Colorful.Console;
+
+namespace GenIVIV {
+    internal sealed class Program {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly ILogger _logger;
+
+        public Program() {
+            Extensions.PrintInformationalHeader();
+
+            var serviceCollection = new ServiceCollection()
+                .AddLavaNode(x => {
+                    x.ReconnectAttempts = 3;
+                    x.ReconnectDelay = TimeSpan.FromSeconds(3);
+                })
+                .AddLogging(x => {
+                    x.ClearProviders();
+                    x.AddProvider(new LoggerProvider());
+                })
+                .AddSingleton<DiscordSocketClient>()
+                .AddSingleton<CommandService>()
+                .AddServices<InjectableServiceAttribute>();
+
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var loggerFactory = _serviceProvider.GetRequiredService<ILoggerFactory>();
+            _logger = loggerFactory.CreateLogger<Program>();
+        }
+
+        private static Task Main() {
+            return new Program().RunAsync();
+        }
+
+        private async Task RunAsync() {
+            await CreateConfigurationAsync();
+            await InitializeDiscordAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private async Task CreateConfigurationAsync() {
+            var databaseService = _serviceProvider.GetRequiredService<DatabaseService>();
+            if (DatabaseService.Exists<ConfigDataModel>("Configuration")) {
+                return;
+            }
+
+            var config = new ConfigDataModel {
+                Id = "Configuration"
+            };
+
+            if (!Extensions.TryTakeInput("Please enter your bot token: ", out var token)) {
+                _logger.LogCritical("A bot token must be provided!");
+                await ExitAsync();
+            }
+
+            config.Token = token;
+            config.Prefix = Extensions.TryTakeInput("Please enter default prefix: ", out var prefix)
+                ? prefix[0]
+                : '?';
+
+            if (Extensions.TryTakeInput("Would you like to enable mention prefix? (Y/N): ", out var enableMention)) {
+                var input = char.ToLower(enableMention[0]);
+                var isEnabled = input == 'y';
+                config.IsMentionEnabled = isEnabled;
+            }
+            
+            if (databaseService.TryStore(config)) {
+                Console.Clear();
+                Extensions.PrintInformationalHeader();
+                return;
+            }
+
+            await ExitAsync();
+        }
+
+        private async Task InitializeDiscordAsync() {
+            var discordService = _serviceProvider.GetRequiredService<DiscordService>();
+            await discordService.InitializeAsync();
+        }
+
+        private async Task ExitAsync() {
+            _logger.LogCritical("No configuration found! Exiting in 5 seconds ...");
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            Environment.Exit(-1);
+        }
+    }
+}
