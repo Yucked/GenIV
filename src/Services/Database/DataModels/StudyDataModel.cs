7c0c0f69d26437e19d8f8fe81f285710cb4db029
Yucked <yucked@protonmail.com>
2020-08-04 22:03:48 -0400

    For machine learning purposes

---
+++ src/Services/Database/DataModels/StudyDataModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using GenIVIV.Services.Database.Interfaces;
+
+namespace GenIVIV.Services.Database.DataModels {
+    public struct StudyDataModel : IDataModel{
+        public string Id { get; set; }
+        public IDictionary<ulong, StudyMessageDataModel> UserMessages { get; set; }
+    }
+
+    public struct StudyMessageDataModel {
+        public string Username { get; set; }
+        public HashSet<string> Messages { get; set; }
+    }
+}
---
+++ src/Services/StudyService.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using GenIVIV.Services.Database;
+using GenIVIV.Services.Database.DataModels;
+
+namespace GenIVIV.Services {
+    public sealed class StudyService {
+        private readonly DiscordSocketClient _socketClient;
+        private readonly DatabaseService _databaseService;
+
+        public StudyService(DiscordSocketClient socketClient, DatabaseService databaseService) {
+            _socketClient = socketClient;
+            _databaseService = databaseService;
+            _socketClient.MessageReceived += OnMessageAsync;
+        }
+
+        private async Task OnMessageAsync(SocketMessage socketMessage) {
+            if (socketMessage.Author.IsBot || socketMessage.Author.IsWebhook) {
+                return;
+            }
+
+            if (!_databaseService.TryGet<StudyDataModel>("Study", out var studyDataModel)) {
+                studyDataModel = new StudyDataModel {
+                    Id = "Study",
+                    UserMessages = new Dictionary<ulong, StudyMessageDataModel>()
+                };
+
+                var studyMessage = new StudyMessageDataModel {
+                    Messages = new HashSet<string>(),
+                    Username = socketMessage.Author.Username
+                };
+                
+                studyMessage.Messages.Add(socketMessage.Content);
+                studyDataModel.UserMessages.Add(socketMessage.Author.Id, studyMessage);
+                _databaseService.TryStore(studyDataModel);
+                return;
+            }
+
+            if (studyDataModel.UserMessages.TryGetValue(socketMessage.Author.Id, out var userMessages)) {
+                userMessages.Messages.Add(socketMessage.Content);
+                _databaseService.TryUpdate(studyDataModel);
+                return;
+            }
+
+            userMessages = new StudyMessageDataModel {
+                Messages = new HashSet<string>(),
+                Username = socketMessage.Author.Username
+            };
+            userMessages.Messages.Add(socketMessage.Content);
+            studyDataModel.UserMessages.TryAdd(socketMessage.Author.Id, userMessages);
+            _databaseService.TryUpdate(studyDataModel);
+        }
+    }
+}
