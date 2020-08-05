53d31067faf6d0354fcc3b8e900187cb6c2468d6
Yucked <yucked@protonmail.com>
2020-08-04 22:15:30 -0400

    Welcome message which no1 cares about

--- src/Services/Database/DataModels/GuildDataModel.cs
+++ src/Services/Database/DataModels/GuildDataModel.cs
@@ -4,5 +4,6 @@ namespace GenIVIV.Services.Database.DataModels {
     public struct GuildDataModel : IDataModel {
         public string Id { get; set; }
         public char Prefix { get; set; }
+        public string WelcomeMessage { get; set; }
     }
 }
